4c13fdc5ec848677692c6b85ce2500164a61a35a
Sean Kallaher <seanp@kallaher.org>
2021-03-29 21:25:05 -0700

    Added protection against receiving None values.

--- bno_tables.py
+++ bno_tables.py
@@ -23,40 +23,40 @@ def write_bno_to_network_table(table, bno):
     # Skip temperature for now
 
     # Put acceleration
-    table.putNumber("accel_x", acceleration[0])
-    table.putNumber("accel_y", acceleration[1])
-    table.putNumber("accel_z", acceleration[2])
+    table.putNumber("accel_x", acceleration[0]) if acceleration[0]
+    table.putNumber("accel_y", acceleration[1]) if acceleration[0]
+    table.putNumber("accel_z", acceleration[2]) if acceleration[0]
 
     # Put magnetic
-    table.putNumber("mag_x", magnetic[0])
-    table.putNumber("mag_y", magnetic[1])
-    table.putNumber("mag_z", magnetic[2])
+    table.putNumber("mag_x", magnetic[0]) if magnetic[0]
+    table.putNumber("mag_y", magnetic[1]) if magnetic[0]
+    table.putNumber("mag_z", magnetic[2]) if magnetic[0]
 
     # Put gyro
-    table.putNumber("gyro_x", gyro[0])
-    table.putNumber("gyro_y", gyro[1])
-    table.putNumber("gyro_z", gyro[2])
+    table.putNumber("gyro_x", gyro[0]) if gyro[0]
+    table.putNumber("gyro_y", gyro[1]) if gyro[0]
+    table.putNumber("gyro_z", gyro[2]) if gyro[0]
 
     # Put euler
-    table.putNumber("euler_roll", euler[0])
-    table.putNumber("euler_pitch", euler[1])
-    table.putNumber("euler_yaw", euler[2])
+    table.putNumber("euler_roll", euler[0]) if euler[0]
+    table.putNumber("euler_pitch", euler[1]) if euler[0]
+    table.putNumber("euler_yaw", euler[2]) if euler[0]
 
     # Put quaternion
-    table.putNumber("quat_x", quaternion[0])
-    table.putNumber("quat_y", quaternion[1])
-    table.putNumber("quat_z", quaternion[2])
-    table.putNumber("quat_w", quaternion[3])
+    table.putNumber("quat_x", quaternion[0]) if quaternion[0]
+    table.putNumber("quat_y", quaternion[1]) if quaternion[0]
+    table.putNumber("quat_z", quaternion[2]) if quaternion[0]
+    table.putNumber("quat_w", quaternion[3]) if quaternion[0]
 
     # Put linear acceleration
-    table.putNumber("lin_accel_x", linear_acceleration[0])
-    table.putNumber("lin_accel_y", linear_acceleration[1])
-    table.putNumber("lin_accel_z", linear_acceleration[2])
+    table.putNumber("lin_accel_x", linear_acceleration[0]) if linear_acceleration[0]
+    table.putNumber("lin_accel_y", linear_acceleration[1]) if linear_acceleration[0]
+    table.putNumber("lin_accel_z", linear_acceleration[2]) if linear_acceleration[0]
 
     # Put gravity
-    table.putNumber("gravity_x", gravity[0])
-    table.putNumber("gravity_y", gravity[1])
-    table.putNumber("gravity_z", gravity[2])
+    table.putNumber("gravity_x", gravity[0]) if gravity[0]
+    table.putNumber("gravity_y", gravity[1]) if gravity[0]
+    table.putNumber("gravity_z", gravity[2]) if gravity[0]
 
 def main():
     logging.info("Starting network tables...")
